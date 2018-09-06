f8784e0dee20959de7e9793064a8200ba92c42e6
Dries Mangelschots <mangelschotsdries@gmail.com>
2018-09-06 14:37:22 +0200

    Added django starter

--- Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
+++ Spring-Examen/src/main/java/edu/ap/spring/controller/JokeController.java
@@ -1,5 +1,9 @@
 package edu.ap.spring.controller;
 
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,13 +15,25 @@ public class JokeController {
    public JokeController() {
    }
        
-   @RequestMapping("/joke")
+   @RequestMapping(value = "/joke", method = RequestMethod.GET)
+   @ResponseBody
    public String joke() {
-	   return "";
+	   
+	   return "Joke";
    }
 		   
    @RequestMapping("/joke_post")
+   @ResponseBody
    public String joke_post() {
+	   URL url;
+	try {
+		url = new URL("http://example.com");
+		   HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		   con.setRequestMethod("GET");
+	} catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	   return "";
    }
    
